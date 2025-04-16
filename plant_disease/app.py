e1f41acd6284173a00ae4a491ed8809f489a9b5a
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 22:41:28 +0530

    Committed streamlit file and the model file

--- plant_disease/app.py
+++ plant_disease/app.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
 
+model = tf.keras.models.load_model('plant_disease_classifier.h5')
+
+class_names = ['Pepper Bell Bacterial Spot', 'Pepper Bell Healthy', 'Potato Early Blight', 'Potato Healthy', 'Tomato Bacterial Spot', 'Tomato Healthy']
+
+st.title("Plant Disease Prediction")
+
+uploaded_file = st.file_uploader("Upload a plant leaf image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = class_names[np.argmax(prediction)]
+
+    st.success(f"Predicted Plant Disease: **{predicted_class}**")
+
+    st.write("Prediction Confidence:")
+    for i, prob in enumerate(prediction[0]):
+        st.write(f"{class_names[i]}: {prob:.2%}")
