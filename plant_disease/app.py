a17c660db4579bed55d4b7766ab60fd784b34921
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 22:42:35 +0530

    Update app.py

--- plant_disease/app.py
+++ plant_disease/app.py
@@ -3,8 +3,10 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
+# Loading the model
 model = tf.keras.models.load_model('plant_disease_classifier.h5')
 
+# Defining the classes of the leaves
 class_names = ['Pepper Bell Bacterial Spot', 'Pepper Bell Healthy', 'Potato Early Blight', 'Potato Healthy', 'Tomato Bacterial Spot', 'Tomato Healthy']
 
 st.title("Plant Disease Prediction")
@@ -15,6 +17,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
+    # Resizing the image to 224 * 224 dimensions in order to match it with the input layer of the model
     img = image.resize((224, 224))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
